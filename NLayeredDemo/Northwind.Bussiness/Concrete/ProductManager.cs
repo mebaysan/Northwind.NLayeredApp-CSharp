e194a78535e3d30bb61fcf3b8abaacac5b3bb7d1
mebaysan <menesbaysan@gmail.com>
2019-05-13 23:38:03 +0300

    fluent validation added

--- NLayeredDemo/Northwind.Bussiness/Concrete/ProductManager.cs
+++ NLayeredDemo/Northwind.Bussiness/Concrete/ProductManager.cs
@@ -1,10 +1,12 @@
 ﻿using Northwind.Bussiness.Abstract;
+using Northwind.Bussiness.ValidationRules.FluentValidation;
 using Northwind.DataAccess.Abstract;
 using Northwind.DataAccess.Concrete;
 using Northwind.DataAccess.Concrete.EntityFramework;
 using Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
@@ -13,7 +15,7 @@ using System.Threading.Tasks;
 namespace Northwind.Bussiness.Concrete
 {
     // burada sadece iş kodları yazılır. Kullanıcı bu datayı görmeye yetkili mi? çektiği data görmesi gereken data mı vs..
-    public class ProductManager:IProductService
+    public class ProductManager : IProductService
     {
         private IProductDal _productDal; // bu sayede hangi veritabanı olursa olsun adapte olacağız
         public ProductManager(IProductDal productDal) // constructor sayesinde productmanager new'lendiğinde bana productdal türünde bir nesne ver
@@ -23,6 +25,9 @@ namespace Northwind.Bussiness.Concrete
 
         public void Add(Product product)
         {
+            ProductValidator productValidator = new ProductValidator();
+            var result = productValidator.Validate(product); // burraya gönderilen ürün için validate işlemlerini uyguladık.
+            
             _productDal.Add(product);
         }
 
@@ -35,7 +40,7 @@ namespace Northwind.Bussiness.Concrete
             }
             catch
             {
-                throw new Exception("Güncelleme Gerçekleşemedi!");   
+                throw new Exception("Güncelleme Gerçekleşemedi!");
             }
         }
 
---
+++ NLayeredDemo/Northwind.Bussiness/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Bussiness.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product> // ürün için kurallar yazacağız
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün ismi boş olamaz"); // ProductName'i boş olamaz
+            RuleFor(p => p.CategoryId).NotEmpty();
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitsInStock).NotEmpty();
+
+            RuleFor(p => p.UnitsInStock).GreaterThanOrEqualTo((short)0); // unit stock = 0'dan büyük olmalı
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(p => p.CategoryId == 2); // unit price category id 2'ye eşit olduğu zaman 10'dan büyük olmalı
+
+        }
+    }
+}
